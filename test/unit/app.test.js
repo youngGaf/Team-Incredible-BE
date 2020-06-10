8782893d27b27b22b7230489735d2bfdb2925426
youngGaf <youngabuhafiz@gmail.com>
2020-06-10 09:41:35 +0100

    Commit on updated unit test login by Gafar_01

---
+++ test/unit/app.test.js
@@ -0,0 +1,30 @@
+const request = require("supertest");
+var should = require("should");
+const expect = require("chai").expect;
+var app = require('../../app');
+
+
+describe('POST api/v1/login', ()=>{
+    //sample correct JSON data which is in database
+    describe('Signing in with a verified email and password', ()=>{
+        it('should respond 200', (done)=>{
+            request(app)
+                .post('/login')
+                .send({username:'boyroberto@gmail.com',password:'emirateboy'})
+                .expect(200)
+                .expect('message', 'Success')
+                done();
+        });
+    });
+    describe('Signing in with an uverified email and password', ()=>{
+        //sample incorrect JSON data which is not in database
+        it('should respond with 404', (done)=>{
+        request.agent(app)
+            .post('/login')
+            .send({username:"incorrect@gmail.com", password:"Patch"})
+                .expect(404)
+                .expect('msg', 'user not found')
+                done();
+        });
+    });  
+});
