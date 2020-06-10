f23b6bfcceb500a94d1e0c730c0290eeb7927fed
youngGaf <youngabuhafiz@gmail.com>
2020-06-10 09:55:51 +0100

    Commit changes to unit login test by Gafar

--- test/unit/app.test.js
+++ test/unit/app.test.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const request = require("supertest");
 var should = require("should");
 const expect = require("chai").expect;
@@ -9,7 +8,7 @@ describe('POST api/v1/login', ()=>{
     //sample correct JSON data which is in database
     describe('Signing in with a verified email and password', ()=>{
         it('should respond 200', (done)=>{
-            request(app)
+            request.agent(app)
                 .post('/login')
                 .send({username:'boyroberto@gmail.com',password:'emirateboy'})
                 .expect(200)
@@ -28,38 +27,4 @@ describe('POST api/v1/login', ()=>{
                 done();
         });
     });  
-=======
-//require assert to confirm test cases
-const assert = require('assert');
-//require functions from app.js
-const app = require('../../app');
-
-
-
-/*READ ME
-This test should simulate the use of API to consume data and 
-push to auth.microapi.dev sucessfully
-using the app() in index.js
-cmd> npm run test 
-
-Note! this test would not pass until the condition is satisfied, 
-modification may still be required. Ref (Gafar_01)*/
-
-describe('POST /login', ()=>{
-    //sample correct JSON data which is in database
-    it('should respond 200', (done)=>{
-        request(app)
-            .post('/api/v1/login')
-            .send({username:'team.incredible@gmail.com',password:'Incredible'})
-            .expect(200)
-            
-    });
-    //sample incorrect JSON data which is not in database
-    it('should respond with 404', (done)=>{
-        request(app)
-            .post('/api/v1/login')
-            .send({username:"incorrect@gmail.com", password:"Patch"})
-            .expect(404)
-    });
->>>>>>> 84297ed9601f331108c086cba5f6aecf2f202723
 });
